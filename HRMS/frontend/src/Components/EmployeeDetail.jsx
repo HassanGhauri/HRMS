e05a99717548a5999f6938d8358f7807d5a81c83
Muhammad Hassan Ghauri <94550154+HassanGhauri@users.noreply.github.com>
2024-08-23 20:17:27 +0500

    Update EmployeeDetail.jsx

--- HRMS/frontend/src/Components/EmployeeDetail.jsx
+++ HRMS/frontend/src/Components/EmployeeDetail.jsx
@@ -2,13 +2,31 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { FaStarAndCrescent } from "react-icons/fa";
-
+import { MdEmail } from "react-icons/md";
+import { MdDriveFileRenameOutline } from "react-icons/md";
+import { TbBrandCashapp } from "react-icons/tb";
+import { TbCategory } from "react-icons/tb";
+import { IoLogOut } from "react-icons/io5";
+import { CiEdit } from "react-icons/ci";
+import { HiUsers } from "react-icons/hi";
 const EmployeeDetail = () => {
   const [employee, setEmployee] = useState([]);
+  const [admins, setAdmins] = useState([])
   const [name, setName] = useState([]);
   const { id } = useParams();
   const navigate = useNavigate();
+  const AdminRecords = () => {
+    axios.get('http://localhost:3000/auth/admin_records')
+    .then(result => {
+      if(result.data.Status) {
+        setAdmins(result.data.Result)
+      } else {
+         alert(result.data.Error)
+      }
+    })
+  }
   useEffect(() => {
+    AdminRecords();
     axios
       .get("http://localhost:3000/employee/detail/" + id)
       .then((result) => {
@@ -37,28 +55,50 @@ const EmployeeDetail = () => {
   };
   return (
     <div>
-      <div className="col  m-0">
-        <h4 className=" d-flex text-dark justify-content-center shadow navbar1">
-        <FaStarAndCrescent /> Employee Management System 
-        </h4>
+      <div className="col p-0 m-0">
+        <p className="p-2 d-flex justify-content-center shadow text-dark navbar">
+        Crescent HR Management System<FaStarAndCrescent />
+        </p>
       </div>
       <div className="d-flex justify-content-center flex-column align-items-center mt-3">
         <img
           src={`http://localhost:3000/Images/` + employee.image}
           className="emp_det_image"
         />
-        <div className="d-flex align-items-center flex-column mt-5">
-          <h3>Name: {employee.name}</h3>
-          <h3>Email: {employee.email}</h3>
-          <h3>Salary: ${employee.salary}</h3>
-          <h3>Field: {name.name}</h3>
+        <div className="d-flex align-items-center flex-column mt-3">
+          <h3><MdDriveFileRenameOutline/> Name: {employee.name}</h3>
+          <h3><MdEmail/> Email: {employee.email}</h3>
+          <h3><TbBrandCashapp/> Salary: ${employee.salary}</h3>
+          <h3><TbCategory/> Field: {name.name}</h3>
         </div>
         <div>
-          <button className="btn bn me-2">Edit</button>
+          <button className="btn bn me-2">Edit <span><CiEdit/></span></button>
           <button className="btn bn" onClick={handleLogout}>
-            Logout
+            Logout <span><IoLogOut/></span>
           </button>
         </div>
+        <div>
+          <h4 p-2 d-flex justify-content-center shadow text-dark >For any queries or leave request, please contact following admins through their emails:</h4>
+        </div>
+        <div className='mt-2 px-5 pt-1'>
+          <h3><HiUsers/> List of Admins</h3>
+          <table className='table'>
+            <thead>
+              <tr>
+                <th>Emails</th>
+              </tr>
+            </thead>
+            <tbody>
+              {
+                admins.map(a => (
+                  <tr>
+                    <td>{a.email}</td>
+                  </tr>
+                ))
+              }
+            </tbody>
+          </table>
+      </div>
       </div>
     </div>
   );
